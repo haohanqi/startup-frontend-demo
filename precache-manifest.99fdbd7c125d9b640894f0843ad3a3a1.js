b78fd9063501a2da771729056edfea7069b3d329
Hao Hanqi <haohanqi@Haos-MacBook-Pro.local>
2019-12-09 14:16:46 -0500

    Updates

---
+++ precache-manifest.99fdbd7c125d9b640894f0843ad3a3a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4499d5005dde949e97a62e64a1467c8",
+    "url": "/startup-frontend-demo/index.html"
+  },
+  {
+    "revision": "42c81049a560c3b685ce",
+    "url": "/startup-frontend-demo/static/css/2.1caa4787.chunk.css"
+  },
+  {
+    "revision": "42c81049a560c3b685ce",
+    "url": "/startup-frontend-demo/static/js/2.5fce03fc.chunk.js"
+  },
+  {
+    "revision": "974234c22292b864f409",
+    "url": "/startup-frontend-demo/static/js/main.fdb03a7a.chunk.js"
+  },
+  {
+    "revision": "7892626548af3f25f42c",
+    "url": "/startup-frontend-demo/static/js/runtime~main.6b68efc1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/startup-frontend-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/startup-frontend-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/startup-frontend-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/startup-frontend-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/startup-frontend-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
